f9ffe887159ff20553aa776399243f4a9a89f8a9
Shandon Vasquez <shandonvasquez1@gmail.com>
2024-04-12 07:26:07 -0600

    This is the change that we have now

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 Command line Interfate = Clipboard
 
 
-Hello, my name is Sheldon and I'm 22 years old. I have a strong passion for the tech field and a sharp eye for detail, which I believe is crucial in a help desk role to ensure smooth computer operations. I'm excellent at solving problems and working independently. Additionally, I love the team environment because we can keep motivated and help other to achieve goals.
+Hello, my name is Sheldon and I'm 22 years old. I have a strong passion for the tech field and a sharp eye for detail, which I believe is crucial in a help desk role to ensure smooth computer operations. I'm excellent at solving problems and working independently.Additionally, I love the team environment because we can keep motivated and help other to achieve goals.
 
-My track record speaks for itself; I consistently exceed goals and 
+My track record speaks for itself; I consistently exceed goals and
 expectations as a top performer in my current role in prior authorization.
  I firmly believe I possess the necessary skills for this role. 
  I know that this role demands someone who is genuinely passionate about 
@@ -11,14 +11,18 @@ expectations as a top performer in my current role in prior authorization.
  committed to keep learning and staying updated on the latest trends 
  and innovations in tech field.
 As reflected in my resume, I'm currently pursuing engineering studies 
-at the university while also actively participating in self-study 
+at the university while also actively participating in self - study 
 and relevant courses.  
 I can warranty if you hired me you won’t regrated, I will be 
 a valuable asset.
 
-While I may not  have everything on the top of my head now however, 
- I know how systems work, and I'm good at figuring out where to search 
- for more information in order to provide with an accurate solution 
+While I may not  have everything on the top of my head now however,
+    I know how systems work, and I'm good at figuring out where to search 
+for more information in order to provide with an accurate solution 
  
 
- Make projects and get credentials 
+ Make projects and get credentials in the tech field is my goal,
+
+
+
+    Hellos this is ehlodn 
